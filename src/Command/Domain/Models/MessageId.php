8c32c5bec5d4e3fa2b7cab69073e108bd641d0fe
Akinori Akatsuka <77688294+akinoriakatsuka@users.noreply.github.com>
2026-01-01 19:29:56 +0900

    Remove deprecated `fromString` and `generate` methods from ID models (GroupChatId, MemberId, UserAccountId, MessageId) in favor of dedicated factory classes.

--- src/Command/Domain/Models/MessageId.php
+++ src/Command/Domain/Models/MessageId.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Akinoriakatsuka\CqrsEsExamplePhp\Command\Domain\Models;
 
 use Akinoriakatsuka\CqrsEsExamplePhp\Infrastructure\Ulid\Ulid;
-use Akinoriakatsuka\CqrsEsExamplePhp\Infrastructure\Ulid\UlidGenerator;
-use Akinoriakatsuka\CqrsEsExamplePhp\Infrastructure\Ulid\UlidValidator;
 
 final readonly class MessageId
 {
@@ -20,26 +18,6 @@ final readonly class MessageId
     public static function from(Ulid $ulid): self
     {
         return new self($ulid);
-    }
-
-    /**
-     * @deprecated Use MessageIdFactory::fromString() instead. This method will be removed in future versions.
-     */
-    public static function fromString(string $value, UlidValidator $validator): self
-    {
-        // プレフィックスが付いている場合は削除
-        if (str_starts_with($value, self::TYPE_PREFIX . '-')) {
-            $value = substr($value, strlen(self::TYPE_PREFIX) + 1);
-        }
-        return new self(Ulid::fromString($value, $validator));
-    }
-
-    /**
-     * @deprecated Use MessageIdFactory::create() instead. This method will be removed in future versions.
-     */
-    public static function generate(UlidGenerator $generator): self
-    {
-        return new self(Ulid::generate($generator));
     }
 
     public function equals(self $other): bool
